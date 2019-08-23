1435b222ad6f3ef3bb423c3d8954394fdaa62736
Leonardo Ribeiro <48728011+LeonardoRibeiro161@users.noreply.github.com>
2019-08-22 21:39:37 -0300

    Add files via upload

---
+++ grafo.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+//Funcao para banner
+void banner()
+{
+	cout<<"-------------------------------------"<<endl;
+	cout<<"Grafo - MATRIZ DE ADJACENCIA"<<endl;
+	cout<<"-------------------------------------"<<endl;
+}
+
+int main()
+{
+	
+	
+	int qte_vert,resp, linha, coluna;
+	banner();
+	
+	cout <<"Informe o numero de Vertices:";
+	cin>>qte_vert;
+	int matriz[qte_vert][qte_vert] = {},i,j;
+	
+	//Mostrar matriz
+	for(i = 0; i < qte_vert; i++)
+	{
+		for(j = 0;j < qte_vert;j++)
+		{
+			cout<<matriz[i][j]<<"\t";
+		}
+		cout<<"\n";
+	}
+	
+	//Preenchendo Matriz
+	do
+	{
+		//Preenchendo as linhas
+		cout<<"Informe a Linha do Vertice:";
+		cin>>linha;
+		
+		//Preenchendo as colunas
+		cout<<"Informe a Coluna do Vertice:";
+		cin>>coluna;
+		matriz[qte_vert][qte_vert] = 1;
+		i++;
+		j++;
+		cout<<"Digite [1] p/ continuar [2] p/ encerrar";
+		cin>>resp;		
+	}
+	while(resp == 1);
+	system("cls");
+	for(i = 0; i < qte_vert; i++)
+	{
+		for(j = 0;j < qte_vert;j++)
+		{
+			cout<<matriz[i][j]<<"\t";
+		}
+		cout<<"\n";
+	}
+	
+}
+
